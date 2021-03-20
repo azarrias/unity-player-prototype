c1a04c1737ca69707d6e000b2755106b051216a0
azarrias <adolfo.zarrias@gmail.com>
2021-03-20 19:52:05 +0100

    Prototype NPC chase behavior

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -7,6 +7,9 @@ public class NPCController : MonoBehaviour
 {
     private NavMeshAgent agent;
     public float PatrolFrequency = 10f;
+    public float AggroRange = 10f;
+    private float patrolSpeed;
+    private float chaseSpeed;
     private const float TICK_FREQUENCY = 0.5f; 
     public Transform[] waypoints;
 
@@ -17,6 +20,8 @@ public class NPCController : MonoBehaviour
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        patrolSpeed = agent.speed / 2;
+        chaseSpeed = agent.speed;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         waypointIndex = Random.Range(0, waypoints.Length);
 
@@ -35,5 +40,11 @@ public class NPCController : MonoBehaviour
     void Tick()
     {
         agent.destination = waypoints[waypointIndex].position;
+        agent.speed = patrolSpeed;
+        if (player != null && Vector3.Distance(transform.position, player.position) < AggroRange)
+        {
+            agent.destination = player.position;
+            agent.speed = chaseSpeed;
+        }
     }
 }
