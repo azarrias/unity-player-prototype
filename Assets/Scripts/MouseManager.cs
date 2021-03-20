1fdc9f1d657a1e92aba8e42df7bee80ce4345925
azarrias <adolfo.zarrias@gmail.com>
2021-03-20 17:11:27 +0100

    Add interactions with other clickable objects such as items

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -11,6 +11,7 @@ public class MouseManager : MonoBehaviour
     public Texture2D normalPointer;
     public Texture2D targetPointer;
     public Texture2D doorwayPointer;
+    public Texture2D combatPointer;
 
     public EventVector3 OnClickEnvironment;
 
@@ -19,10 +20,16 @@ public class MouseManager : MonoBehaviour
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit raycastHit, 50, clickableLayer.value))
         {
             bool door = false;
+            bool item = false;
             if (raycastHit.collider.gameObject.tag == "Doorway")
             {
                 Cursor.SetCursor(doorwayPointer, new Vector2(16, 16), CursorMode.Auto);
                 door = true;
+            }
+            else if (raycastHit.collider.gameObject.tag == "Item")
+            {
+                Cursor.SetCursor(combatPointer, new Vector2(16, 16), CursorMode.Auto);
+                item = true;
             }
             else
             {
@@ -31,7 +38,22 @@ public class MouseManager : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
-                OnClickEnvironment.Invoke(raycastHit.point);
+                if (door)
+                {
+                    Transform doorway = raycastHit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(doorway.position);
+                    Debug.Log("DOOR");
+                }
+                else if (item)
+                {
+                    Transform itemTransform = raycastHit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(itemTransform.position);
+                    Debug.Log("ITEM");
+                }
+                else
+                {
+                    OnClickEnvironment.Invoke(raycastHit.point);
+                }
             }
         }
         else
