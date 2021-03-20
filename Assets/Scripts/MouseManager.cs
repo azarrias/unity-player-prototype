68986b52d38ad7f962ac523eb189cfd840e52e8b
azarrias <adolfo.zarrias@gmail.com>
2021-03-20 13:29:53 +0100

    Prototype cursor interaction with doors and other clickable objects

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    // Know what objects are clickable
+    public LayerMask clickableLayer;
+
+    // Swap cursors per object
+    public Texture2D normalPointer;
+    public Texture2D targetPointer;
+    public Texture2D doorwayPointer;
+
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit raycastHit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if (raycastHit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorwayPointer, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+            else
+            {
+                Cursor.SetCursor(targetPointer, new Vector2(16, 16), CursorMode.Auto);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(normalPointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
